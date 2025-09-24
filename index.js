1561cddc66e98e8517ba30ccc89c15ab16b574ea
mizuiro14cpu <yoseforiel.borbon-22@cpu.edu.ph>
2025-09-24 17:12:22 +0800

    feat: basic features for assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT || 3000
+app
+    .use(cors())
+    .use(express.json())
+    .use(express.urlencoded({extended: true}))
+    .post('/login', (req, res) => {
+        console.log(req.body)
+        res.send(req.body)
+    })
+    .listen(PORT, () => {
+        console.log("Server has started at http://localhost:3000")
+    })
+
